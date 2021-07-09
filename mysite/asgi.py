aa17615c7e43beb7fb96a8ec53546bc93443452e
xqcshane <83743200+xqcshane@users.noreply.github.com>
2021-07-09 22:07:36 +0800

    Add files via upload

--- mysite/asgi.py
+++ mysite/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for mysite project.
+ASGI config for mysite2 project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
--- mysite/urls.py
+++ mysite/urls.py
@@ -1,4 +1,4 @@
-"""mysite URL Configuration
+"""mysite2 URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
@@ -14,12 +14,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('blog.urls')),
-    path('item/', include('item.urls')),
-    path('iot/', include('iot.urls')),
-    path('dashboard/', include('dashboard.urls')),
 ]
